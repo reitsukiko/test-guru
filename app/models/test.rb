ee2138a929efe1a2727b14662ccc05f5b82a3f36
Albina <reitsukiko@yandex.ru>
2021-12-23 02:46:01 +0300

    Lesson4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category)
+    Test.joins("JOIN categories ON tests.category_id = categories.id")
+        .where(categories: { title: category })
+        .order('tests.title desc')
+        .pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def find_tests_by_level(level)
+    tests.where(level: level)
+  end
 end
