301a9237cbec6a3636c2e6748540706208e1cd74
Albina <reitsukiko@yandex.ru>
2021-12-23 17:15:15 +0300

    les4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def find_tests_by_level(level)
-    tests.where(level: level)
+    Test.joins('JOIN passings ON passings.tests_id = tests.id')
+        .where(passings: {user_id: id}, tests: {level: level})
   end
 end
