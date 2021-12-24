a537039f065500d62215868c0b5899f9470f115f
Albina <reitsukiko@yandex.ru>
2021-12-24 21:18:36 +0300

    les5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,18 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  belongs_to :author, inverse_of: 'author_tests'
+
+  has_many :question, dependent: :destroy
+
+  has_many :passings, dependent: :destroy
+
+  has_many :users, through: :passings
+
   def self.tests_by_category(category)
-    Test.joins("JOIN categories ON tests.category_id = categories.id")
-        .where(categories: { title: category })
-        .order('tests.title desc')
-        .pluck(:title)
+    joins(:category)
+      .where(categories: { title: category })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
