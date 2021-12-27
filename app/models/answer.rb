f7f460b0865b335526d736928a7bfc9b23b45168
Albina <reitsukiko@yandex.ru>
2021-12-27 04:14:43 +0300

    les6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,14 @@
 class Answer < ApplicationRecord
-  belongs_to :question 
+  belongs_to :question
+
+  validates :body, presence: true
+  validate :count_of_answers, on: :create
+
+  scope :correct_answer, -> { where(correct: true) }
+
+  private
+
+  def count_of_answers
+    errors.add(:base, 'Limit violated, min 1 answer, max 4 answer') if question.answers.count > 3
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_080113) do
+ActiveRecord::Schema.define(version: 2021_12_27_010624) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_12_23_080113) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"
