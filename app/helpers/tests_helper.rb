41c8ac49be9a3cb3b177f594a83ef50cbe952b6b
Albina <reitsukiko@yandex.ru>
2022-02-14 17:32:38 +0300

    les8

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,11 @@
 module TestsHelper
+  TEST_LEVELS = { '0..1' => :easy, '2..4' => :normal, 5 => :hard }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
+  end
+
+  def test_questions_count(test)
+  	test.questions.count
+  end
 end
