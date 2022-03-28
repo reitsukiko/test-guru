b4f85eea5e000f0f2bdb259188c72316a8fe169a
Albina <reitsukiko@yandex.ru>
2022-03-28 18:34:44 +0300

    les10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :test_passages, dependent: :destroy
 
   has_many :tests, through: :test_passages
 
   has_many :сreated_tests, class_name: 'Test', foreign_key: :author_id
 
-  validates :email, :name, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 
   def find_tests_by_level(level)
     tests.where(level: level)
@@ -14,4 +21,5 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_100758) do
+ActiveRecord::Schema.define(version: 2022_03_24_225257) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_100758) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
