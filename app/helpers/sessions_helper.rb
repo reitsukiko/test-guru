d01d5ef253b78b98aa067aa29f03544fc7926bb7
Albina <reitsukiko@yandex.ru>
2022-03-30 01:09:53 +0300

    les10.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
 
   def flash_message
-    flash.each do |key, msg|
+    flash.map do |key, msg|
       content_tag :p, msg, class: 'flash #{key}'
     end.join.html_safe
   end
-  
+
 end
