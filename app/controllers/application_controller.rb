23c4b6f4e2f90aa72a7b46be7af3e3d3c3832e99
Albina <reitsukiko@yandex.ru>
2022-04-08 01:48:12 +0300

    les11.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user!
+  before_filter do 
+    redirect_to admin_tests_path if current_user.admin?
+  end
+
 end
